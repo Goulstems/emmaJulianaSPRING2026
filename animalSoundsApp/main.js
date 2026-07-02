2befec2a808f2ebde4678bcad2e73bc75611fca7
Goulstem <sk84life4evr@Gmail.com>
2026-07-01 18:52:24 -0700

    prompt

--- animalSoundsApp/main.js
+++ animalSoundsApp/main.js
@@ -19,6 +19,7 @@ let sounds = {
 };
 
 //Elements on the screen
+const inputPrompt = document.getElementById("inputPrompt");
 const input = document.getElementById("animalNameInput");
 const button = document.getElementById("submit");
 const feedback = document.getElementById("animalSoundFeedback");
@@ -33,3 +34,12 @@ button.addEventListener("click",()=>{
     audio.play()
     feedback.textContent = animalSoundsText[userInput];
 });
+
+//LOAD on the screen, prompt of animal name choices!
+//LOOP through one of the data dictionaries!
+    //For now console log.
+    //TODO: Actually append to the screen.
+
+for (let animal in sounds){
+    inputPrompt.innerHTML += animal+" is an option!\n";
+}
